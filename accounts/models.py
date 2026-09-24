6fa92c2763c4529cabc80e904ea68d9bfea0dc5b
CEAdlin <thehhomestead@gmail.com>
2026-09-24 20:11:54 +0100

    Sync customer profile model migration

--- accounts/models.py
+++ accounts/models.py
@@ -2,11 +2,20 @@
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
+
 
 class CustomerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=120)
-    address = models.TextField()
+    address = models.TextField(
+        validators=[
+            MinLengthValidator(
+                20,
+                "Address must be at least 20 characters.",
+            )
+        ]
+    )
     phone_number = models.CharField(max_length=20)
 
     def __str__(self):
